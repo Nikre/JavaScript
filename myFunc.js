8ffa669acb69011848eaa2a650e749e72fda10a8
Nico <monarca.nicolo@gmail.com>
2020-04-27 13:28:56 +0200

    [ADD] Verified the passed variable

--- myFunc.js
+++ myFunc.js
@@ -2,6 +2,18 @@ function logMessage(){
     console.log("message");
 }
 
+// This is  another method for declaring a function
 let anotherLogMessage = function(){
     console.log("message2");
 }
+
+let num = 0;
+//I'm verifing that the reference passing 
+function adder(number){
+    number += 2;
+    console.log(number);
+}
+
+console.log(num);
+adder(num)
+console.log(num);
